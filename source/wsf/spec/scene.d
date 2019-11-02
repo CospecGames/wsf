46efe3d59b8facc70124da3bc2493482e28a7bc7
Member1221 <clipseypone@gmail.com>
2019-11-02 02:20:01 +0100

    Basic scene format layout

--- source/wsf/spec/scene.d
+++ source/wsf/spec/scene.d
@@ -6,29 +6,117 @@
 
 module wsf.spec.scene;
 
-// TODO: Implement
-
+/**
+    The scene in-game
+*/
 class Scene {
+    /**
+        Contains all the tilesets for this scene
+    */
     Tileset[string] tilesets;
+
+    /**
+        Width of tiles
+    */
+    uint tileWidth;
+
+    /**
+        Height of tiles
+    */
+    uint tileHeight;
+
+    /**
+        The amount of tile slots in the X axis
+    */
+    uint tilesX;
+
+    /**
+        The amount of tile slots on the Y axis
+    */
+    uint tilesY;
+
+    /**
+        Layers
+
+        index 0 = main layer
+    */
+    Layer[] layers;
 }
 
+/**
+    A tileset
+*/
 struct Tileset {
+    /**
+        Width of tileset texture
+    */
     uint width;
+
+    /**
+        Height of tileset texture
+    */
     uint height;
+
+    /**
+        8-bit RGBA color data
+    */
     ubyte[] data;
 }
 
+/**
+    A layer
+*/
 class Layer {
-    
+    /**
+        Contains all the tiles for this layer
+    */
+    Tile[] tiles;
+
+    /**
+        Wether to draw the layer infront or behind the main layer
+    */
+    LayerOrder order;
 }
 
+/**
+    Draw order for layer
+*/
+enum LayerOrder : ubyte {
+    Auto = 0,
+    Background = 1,
+    Foreground = 2
+}
+
+/**
+    Tile flip
+*/
 enum TileFlip : ubyte {
     None,
     Horizontal = 1,
     Vertical = 3
 }
 
+/**
+    A tile
+*/
 struct Tile {
+    /**
+        X coordinate of tile
+    */
+    uint x;
+
+    /**
+        T coordinate of tile
+    */
+    uint y;
+
+    /**
+        ID of tile
+    */
     ubyte id;
+
+    /**
+        Which direction to flip tile
+    */
     TileFlip flip;
 }
